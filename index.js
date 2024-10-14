dacb68f5fcfde768c6a2f4d9bad6f8517d3b2f75
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-14 16:07:05 +0200

    ADD: modifico il biglietto degli over 65

--- index.js
+++ index.js
@@ -8,4 +8,31 @@ console.log('js-biglietto-treno')
 //L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
+//[alert (ciao utente!)]
+//acquisizione dei dati
+const userAge = prompt ('inserisci la tua età')
+const OtherAge= parseInt (prompt("inserisci l'eta di un'altro utente."))
+
+const numberOtherAge = 
+
+//costo biglietto
+console.log (200 * 0.21)
+
+//biglietto biglietto scontato per minorenni
+
+let = ''
+
+if (userAge < 18) {
+    sconto = (42 * 20) / 100
+}
+
+//biglietto scontato per over 65
+
+else (otherAge ==> 65) {    
+    sconto = (42 * 40 ) / 100
+}
+
+
+
+
 
